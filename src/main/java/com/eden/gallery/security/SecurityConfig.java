f930bd1bf0207ce9f1a5120743c07a667e8ff971
nvh-gel <nvhien2703@outlook.com>
2023-04-24 02:06:08 +0700

    GAL-42 update cors config

--- src/main/java/com/eden/gallery/security/SecurityConfig.java
+++ src/main/java/com/eden/gallery/security/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.eden.gallery.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 /**
  * Security configuration for application.
@@ -18,6 +22,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+
+    private static final List<String> ALLOW_ORIGINS = List.of(
+            "http://localhost:3000"
+    );
+    private static final List<String> ALLOW_METHODS = List.of("GET", "PUT", "POST", "OPTIONS");
 
     private JwtTokenFilter jwtTokenFilter;
 
@@ -39,14 +48,30 @@ public class SecurityConfig {
                 .permitAll();
 
         http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/user", "/auth/login").anonymous()
+                .requestMatchers("/user", "/auth/login").anonymous()
                 .requestMatchers("/", "/healthz").anonymous()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.cors();
 
         return http.build();
+    }
+
+    /**
+     * CORS configuration for front end API.
+     *
+     * @return cors config source
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ALLOW_ORIGINS);
+        configuration.setAllowedMethods(ALLOW_METHODS);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     /**
