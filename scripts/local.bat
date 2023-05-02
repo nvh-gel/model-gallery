77bfb529331834c3d6c30ce83dcc38f077f30a76
nvh-gel <nvhien2703@outlook.com>
2023-05-02 12:06:33 +0700

    update main branch for deployment

--- scripts/local.bat
+++
@@ -1,7 +0,0 @@
-minikube kubectl -- apply --filename local.yml
-
-minikube kubectl -- expose deployment model-gallery --type=NodePort --port=8080
-
-minikube service model-gallery
-
-@REM minikube kubectl -- port-forward service/model-gallery 8080:8080
