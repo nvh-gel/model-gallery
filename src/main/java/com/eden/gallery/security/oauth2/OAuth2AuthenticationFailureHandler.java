08381c7a7a23bd8d86507efa7779b49dfdbf91a6
nvh-gel <nvhien2703@outlook.com>
2023-05-08 23:08:49 +0700

    WIP: GAL-56 google login api

--- src/main/java/com/eden/gallery/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/eden/gallery/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,8 +1,43 @@
 package com.eden.gallery.security.oauth2;
 
+import com.eden.gallery.utils.CookieUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+
+import static com.eden.gallery.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
+@AllArgsConstructor
+@Log4j2
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private HttpCookieOAuth2AuthorizationRequestRepository oAuth2AuthorizationRequestRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse("/");
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", exception.getLocalizedMessage())
+                .build().toUriString();
+
+        oAuth2AuthorizationRequestRepository.removeAuthorizationRequest(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
 }
