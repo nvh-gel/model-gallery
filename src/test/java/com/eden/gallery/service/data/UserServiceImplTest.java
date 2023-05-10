2f198a5389e409294ee37709d3e0bcde79bfbcb1
nvh-gel <nvhien2703@outlook.com>
2023-05-10 14:17:51 +0700

    GAL-56 update unit test

---
+++ src/test/java/com/eden/gallery/service/data/UserServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.eden.gallery.service.data;
+
+import com.eden.gallery.repository.sql.UserRepository;
+import com.eden.gallery.service.UserService;
+import com.eden.gallery.service.impl.UserServiceImpl;
+import com.eden.gallery.viewmodel.UserVM;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+
+@DataJpaTest
+class UserServiceImplTest {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Test
+    void testFindAllUsers() {
+
+        Page<UserVM> result = userService.findAll(0, 10);
+        Assertions.assertEquals(0, result.getTotalElements());
+        Assertions.assertEquals(0, result.getPageable().getPageNumber());
+
+        result = userService.findAll(5, 10);
+        Assertions.assertEquals(0, result.getTotalElements());
+        Assertions.assertEquals(4, result.getPageable().getPageNumber());
+
+    }
+
+    @TestConfiguration
+    public static class UserServiceTestContext {
+
+        @Autowired
+        private UserRepository userRepository;
+
+        @Bean
+        public UserService userService() {
+            return new UserServiceImpl(userRepository,
+                    null,
+                    null,
+                    null);
+        }
+    }
+}
--- src/test/java/com/eden/gallery/service/impl/ModelServiceImplTest.java
+++ src/test/java/com/eden/gallery/service/impl/ModelServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.eden.gallery.service.impl;
 
 import com.eden.gallery.model.Model;
+import com.eden.gallery.producer.ModelProducer;
 import com.eden.gallery.repository.sql.ModelRepository;
 import com.eden.gallery.service.ModelService;
 import com.eden.gallery.viewmodel.ModelVM;
+import com.eden.queue.util.Action;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,11 +22,13 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @ExtendWith(SpringExtension.class)
 class ModelServiceImplTest {
@@ -33,19 +37,21 @@ class ModelServiceImplTest {
     private ModelServiceImpl modelService;
     @MockBean
     private ModelRepository modelRepository;
+    @MockBean
+    private ModelProducer modelProducer;
 
     @BeforeEach
     void setUp() {
         modelService.setModelRepository(modelRepository);
+        modelService.setModelProducer(modelProducer);
 
-        Mockito.when(modelRepository.findById(999L))
-                .thenReturn(Optional.empty());
+        Mockito.when(modelRepository.findById(999L)).thenReturn(Optional.empty());
         Model model = new Model();
         model.setId(1L);
-        Mockito.when(modelRepository.findById(1L))
-                .thenReturn(Optional.of(model));
-        Mockito.when(modelRepository.save(any()))
-                .thenReturn(model);
+        Mockito.when(modelRepository.findById(1L)).thenReturn(Optional.of(model));
+        Mockito.when(modelRepository.save(any())).thenReturn(model);
+        Mockito.when(modelProducer.sendMessageToQueue(eq(Action.DELETE), any()))
+                .thenReturn(UUID.randomUUID().toString());
     }
 
     @Test
@@ -93,6 +99,14 @@ class ModelServiceImplTest {
     void testDeleteSuccess() {
         ModelVM result = modelService.delete(1L);
         assertNotNull(result);
+    }
+
+    @Test
+    void testDeleteOnQueueSuccess() {
+        String result = modelService.deleteOnQueue(1L);
+        assertNotNull(result);
+        UUID uuid = UUID.fromString(result);
+        assertNotNull(uuid);
     }
 
     @TestConfiguration
